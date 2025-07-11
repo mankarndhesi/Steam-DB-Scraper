8d2275742eb5f37eb19c372314e565b60f4a61df
mankarndhesi <mankarndhesi@gmail.com>
2025-07-11 14:06:48 -0700

    Project structure established

---
+++ steam-scraper-backend/src/main/java/com/mdhes/steamscraper/steam_scraper_backend/SteamScraperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mdhes.steamscraper.steam_scraper_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamScraperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamScraperBackendApplication.class, args);
+	}
+
+}
---
+++ steam-scraper-backend/src/test/java/com/mdhes/steamscraper/steam_scraper_backend/SteamScraperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdhes.steamscraper.steam_scraper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamScraperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
